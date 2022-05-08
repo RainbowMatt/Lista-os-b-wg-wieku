e200244152107e6b6bd694f2e8a0e7d0f3ae698e
Mateusz.Witulski <Mateusz.Witulski@PK5-15>
2022-05-08 12:16:25 +0200

    Dodaj pliki projektów.

---
+++ Lista osób wg wieku/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lista_osób_wg_wieku
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
